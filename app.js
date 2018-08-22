dee8254ad43b5b1e3feab1069f24f12500809139
Rafael Dias <ruds.devel@gmail.com>
2018-08-22 17:49:07 -0300

    FIX

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
 "use strict";
 
 var app = require('./app/configs/express/express');
-var wsSocket = require('./app/sockets/wsServerPeople').peopleSocket;
 var server = app.listen(3000,()=>console.log('Server running...'));
-
-wsSocket(server);
