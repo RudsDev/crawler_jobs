3bcacac21201b4044cf604606d79fa212d691d32
Rafael Dias <ruds.devel@gmail.com>
2018-09-24 16:33:32 -0300

    Add primeira página.

--- app/routes/crawler.js
+++ app/routes/crawler.js
@@ -3,7 +3,7 @@
 const router = require('express').Router();
 const CrawlerController = require('./../controllers/CrawlerController');
 const crawlerController = new CrawlerController();
-const MAX_PAGES = 3;
+const MAX_PAGES = 4;
 
 router.get('/:value', async function(req, res) {
 	res.send(await crawlerController.run(MAX_PAGES));
